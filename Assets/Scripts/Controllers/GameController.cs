d3073ef2f0abb0ef5738bf6d0ad3e9d85549280b
elborio <elborio@hotmail.com>
2015-06-25 14:06:16 +0200

    Started Game Controller

---
+++ Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+	public enum gameState
+	{
+		CHOOSING,
+		FIGHTING,
+		TRADING,
+		LOOTING,
+		WAITING
+	}
+
+	public enum eventType
+	{
+		TREASUREROOM,
+		STORY,
+		LEGENDARYMONSTER,
+		MONSTER
+	}
+}
