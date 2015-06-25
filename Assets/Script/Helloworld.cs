66c8789f28aa2d82e8535b96dba17dcaa61d9bb5
elborio <elborio@hotmail.com>
2015-06-25 12:54:46 +0200

    Test upload hello world
    je snapt het wel.

---
+++ Assets/Script/Helloworld.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Helloworld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hello World");
+	}
+}
